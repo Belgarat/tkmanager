3f223aa4405fb24a26f96cb4ea2fbffb29778220
DeepCode <viacart@gmail.com>
2017-02-17 10:34:51 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var morgan      = require('morgan');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var users = require('./routes/users');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser())
+app.use(morgan('dev'));
+
+app.use('/api/v1/users', users);
+
+module.exports = app;
