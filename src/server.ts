11613f922809729aab7bab5c015fca572220db26
DeepCode <viacart@gmail.com>
2017-04-15 08:38:21 +0200

    server: add header to enable Access-Control-Allow-Origin

--- src/server.ts
+++ src/server.ts
@@ -95,6 +95,22 @@ export class Server {
         next(err);
     });
 
+    // Add headers
+    this.app.use(function (req, res, next) {
+
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', '*');
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+        // Pass to next layer of middleware
+        next();
+    });
+
     //error handling
     this.app.use(errorHandler());
   }
